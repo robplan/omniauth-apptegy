6aaaaabb279aacbb3543ae6dd018a6b980160bee
Roberto Plancarte <roberto.plancarte@apptegy.com>
2020-11-14 19:39:11 -0600

    Apptegy config

--- lib/omniauth/apptegy.rb
+++ lib/omniauth/apptegy.rb
@@ -1,8 +1,44 @@
 require "omniauth/apptegy/version"
+require 'omniauth-oauth2'
 
-module Omniauth
-  module Apptegy
-    class Error < StandardError; end
-    # Your code goes here...
+module OmniAuth
+  module Strategies
+    class Apptegy < OmniAuth::Strategies::OAuth2
+      # Give your strategy a name.
+      option :name, "apptegy"
+
+      # This is where you pass the options you would pass when
+      # initializing your consumer from the OAuth gem.
+      option :client_options, {:site => "https://localhost"}
+      
+      # You may specify that your strategy should use PKCE by setting
+      # the pkce option to true: https://tools.ietf.org/html/rfc7636
+      option :pkce, false
+
+      # These are called after authentication has succeeded. If
+      # possible, you should try to set the UID without making
+      # additional calls (if the user id is returned with the token
+      # or as a URI parameter). This may not be possible with all
+      # providers.
+      uid{ raw_info['id'] }
+
+      info do
+        {
+          :id => raw_info['id'],
+          :name => raw_info['name'],
+          :email => raw_info['email']
+        }
+      end
+
+      extra do
+        {
+          'raw_info' => raw_info
+        }
+      end
+
+      def raw_info
+        @raw_info ||= access_token.get('/userinfo').parsed
+      end
+    end
   end
 end
